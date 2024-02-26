0f6c3387f5c0482e866fef46a89fbe087c9cce36
Jyothis <jyothisv@gmx.com>
2024-02-25 19:45:10 -0500

    New tabs should visit the scratch buffer

--- core/bono-core-ui.el
+++ core/bono-core-ui.el
@@ -17,7 +17,7 @@
 ;; Enable the tab-bar
 (tab-bar-mode)
 
-;; (setq tab-bar-new-tab-choice 'window)
+(setq tab-bar-new-tab-choice "*scratch*")
 (setq tab-bar-new-tab-to 'rightmost)
 
 ;; C-c <left>/<right> for cycling through tab bars
